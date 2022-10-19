ead0d5cf32dcade37c3ed08a656fe3d19073238e
POOJA BHOSKAR <72018117+bhoskarpooja@users.noreply.github.com>
2022-10-19 15:27:11 +0530

    Update zigZagSequence.cs

--- Add Code Here/C#/zigZagSequence.cs
+++ Add Code Here/C#/zigZagSequence.cs
@@ -1,19 +1,20 @@
-public static void zigZagSequesnce()
+using systems;
+public class Zigzag
+{
+
+ public static void zigZagSequesnce()
         {
             int[] arr = new int[] { 2, 6, 1, 4, 7, 3, 5 };
             int length = arr.Length;
             int midIndex = length / 2;
             int lastIndex = length - 1;
-
-            //Step 1 - Sort
+            
             Array.Sort(arr);
-
-            //Step 2 - Swap largest element into the middle
+            
             int max = arr[lastIndex];
-            arr[lastIndex] = arr[midIndex]; //7 / 2 = 3.5, 3
+            arr[lastIndex] = arr[midIndex]; 
             arr[midIndex] = max;
-
-            //Step 3 - Reverse remaining elements
+            
             int leftIndex = midIndex + 1;
             int rightIndex = lastIndex - 1;
 
@@ -26,7 +27,7 @@ public static void zigZagSequesnce()
                 leftIndex++;
                 rightIndex--;
             }
-
             Console.WriteLine(string.Join(",", arr));
             Console.ReadLine();
         }
+}
